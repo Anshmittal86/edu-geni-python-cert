b8e3fad8b60577f0afb6d8d37be7e14f0619b9a5
Ansh Mittal <anshmit657@gmail.com>
2026-07-08 14:56:27 +0530

    feat: implement researcher and writer tools with Tavily integration

--- langchain/agents/research-and-write-agent-using-create-agent-react.py
+++ langchain/agents/research-and-write-agent-using-create-agent-react.py
@@ -1,20 +1,36 @@
 from langchain.agents import create_agent
-from langchain.messages import HumanMessage
+from langchain.messages import HumanMessage, ToolMessage
 from langchain.tools import tool
 from dotenv import load_dotenv
-import requests
+from tavily import TavilyClient
+import os
 
 load_dotenv()
 
-@tool
-def researcher(topic: str) -> str:
-    """ Take topic as an parameter and give the latest research of the topic. """
-    return "research is coming soon..."
+api_key = os.getenv("TAVILY_API_KEY")
+
+tavily_client = TavilyClient(api_key=api_key)
 
 @tool
-def writer(facts: str) -> str:
+def researcher(topic: str) -> str:
+    """Take topic as an parameter and give the latest research of the topic."""
+    try:
+        response = tavily_client.search(topic)
+    except Exception as e:
+        print(f'Something is wrong: {str(e)}')
+    return response
+
+@tool
+def writer(researcher_response: str) -> str:
     """ Take facts as an parameter and write a polished summary"""
-    return "writer is coming soon..."
+    
+    writer_result = agent.invoke({
+        "messages": [
+            ToolMessage(researcher_response)
+        ]
+    })
+    
+    return writer_result
 
 agent = create_agent(
     model='gpt-4o-mini',
